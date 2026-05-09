c20a5941319cf1efba224d68b55ab36a57b06c5a
Ruben Dario Cabrera Garcia <rubentxu74@gmail.com>
2026-05-10 00:05:57 +0200

    threshold_tune(S4792): adjust detection threshold [auto]

--- crates/cognicode-axiom/src/rules/catalog.rs
+++ crates/cognicode-axiom/src/rules/catalog.rs
@@ -357,8 +357,9 @@ declare_rule! {
         let mut issues = Vec::new();
         let weak_patterns = [
             (r"(?i)(?:\b|_)md5\b", "MD5 hash function"),
-            (r"(?i)(?:\b|_)sha1\b", "SHA-1 hash function"),
-            (r"(?i)(?:\b|_)des(?:ign|esst|ktop|tails)?\b", "DES block cipher"),
+            (r"(?i)(?:\b|_)sha1?0?\b", "SHA-0/SHA-1 hash function"),
+            (r"(?i)(?:\b|_)des\b", "DES block cipher"),
+            (r"(?i)(?:\b|_)3des\b", "Triple DES (3DES) block cipher"),
             (r"(?i)(?:\b|_)rc4\b", "RC4 stream cipher"),
             (r"(?i)(?:^|[^\w])crypt\b", "crypt(3) function"),
         ];
