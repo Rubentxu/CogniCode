fe067199480f3246b1f5c7e11a67204106952982
Ruben Dario Cabrera Garcia <rubentxu74@gmail.com>
2026-05-10 00:13:51 +0200

    chore(lint): tune threshold for [auto]

--- crates/cognicode-axiom/src/rules/catalog.rs
+++ crates/cognicode-axiom/src/rules/catalog.rs
@@ -356,11 +356,11 @@ declare_rule! {
     check: => {
         let mut issues = Vec::new();
         let weak_patterns = [
-            (r"(?i)(?:\b|_)md5\b", "MD5 hash function"),
-            (r"(?i)(?:\b|_)sha[01]\b", "SHA-0/SHA-1 hash function"),
-            (r"(?i)(?:\b|_)des\b", "DES block cipher"),
-            (r"(?i)(?:\b|_)3des\b", "Triple DES (3DES) block cipher"),
-            (r"(?i)(?:\b|_)rc4\b", "RC4 stream cipher"),
+            (r"(?i)(?:\b|_|crypt)md5\b", "MD5 hash function"),
+            (r"(?i)(?:\b|_|crypt)sha[01]\b", "SHA-0/SHA-1 hash function"),
+            (r"(?i)(?:\b|_|crypt)des\b", "DES block cipher"),
+            (r"(?i)(?:\b|_|crypt)3des\b", "Triple DES (3DES) block cipher"),
+            (r"(?i)(?:\b|_|crypt)rc4\b", "RC4 stream cipher"),
             (r"(?i)(?:^|[^\w])crypt\b", "crypt(3) function"),
         ];
 
