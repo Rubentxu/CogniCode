bf66b281a040d1453bc654b0ba400f2adef77d4d
Ruben Dario Cabrera Garcia <rubentxu74@gmail.com>
2026-05-10 00:22:36 +0200

    refactor(S4792): improve [auto]

--- crates/cognicode-axiom/src/rules/catalog.rs
+++ crates/cognicode-axiom/src/rules/catalog.rs
@@ -361,7 +361,7 @@ declare_rule! {
         let weak_patterns = [
             (r"(?i)(?:\b|_)(?:hash|digest|sign|encrypt)_?md5\b", "MD5 hash function"),
             (r"(?i)(?:\b|_)(?:hash|digest|sign|encrypt)_?sha[01]\b", "SHA-0/SHA-1 hash function"),
-            (r"(?i)(?:^|[^\\w])(?:_)?des(?:_|$|[^\\w])", "DES block cipher"),
+            (r"(?i)(?:^|[^\w])(?:_)?des(?:_|$|[^\w])", "DES block cipher"),
             (r"(?i)(?:\b|_)3des\b", "Triple DES (3DES) block cipher"),
             (r"(?i)(?:\b|_)rc4\b", "RC4 stream cipher"),
             (r"(?i)(?:^|[^\w])crypt\b", "crypt(3) function"),
